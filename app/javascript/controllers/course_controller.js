ae91087a75825a645fc7bd25a20677f77eb949c3
Roonglit Chareonsupkul <roonglit@gmail.com>
2025-09-24 22:25:08 +0700

    try adding filepond into the form

---
+++ app/javascript/controllers/course_controller.js
@@ -0,0 +1,55 @@
+import { Controller } from "@hotwired/stimulus"
+import { DirectUpload } from '@rails/activestorage'
+import * as FilePond from "filepond"
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+
+export default class extends Controller {
+  static targets = ["filepond"]
+
+  connect() {
+    this.filepondTarget && this.initializeFilePond(
+      this.filepondTarget.dataset.directUploadUrl
+    )
+  }
+
+  initializeFilePond(directUploadUrl) {
+    console.log("upload url:", directUploadUrl)
+    FilePond.registerPlugin(FilePondPluginImagePreview);
+
+    const filepond = FilePond.create(this.filepondTarget, {
+      server: {
+        process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+          const upload = new DirectUpload(file, directUploadUrl, {
+            directUploadWillStoreFileWithXHR(request) {
+              console.log("directUploadWillStoreFileWithXHR", request)
+              request.upload.addEventListener("progress",
+                event => progress(event.lengthComputable, event.loaded, event.total)
+              );
+            }
+          });
+
+          upload.create((error, blob) => {
+            if (error) {
+              // Handle the error
+            } else {
+              // Add an appropriately-named hidden input to the form with a
+              //  value of blob.signed_id so that the blob ids will be
+              //  transmitted in the normal upload flow
+
+              // const hiddenField = document.createElement('input')
+              // hiddenField.setAttribute("type", "hidden");
+              // hiddenField.setAttribute("value", blob.signed_id);
+              // console.log("input:", this.filepondTarget);
+              // hiddenField.name = input.name
+              // document.querySelector('form').appendChild(hiddenField)
+            }
+          })
+
+          return {
+            abort: () => abort()
+          }
+        }
+      }
+    })
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,10 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.0.300
+pin "@rails/actiontext", to: "actiontext.esm.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
-pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "filepond" # @4.32.9
+pin "filepond-plugin-image-preview" # @4.6.12
+
